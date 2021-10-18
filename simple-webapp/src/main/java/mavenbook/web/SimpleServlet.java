00aef126517aec5dc8f8a6f157ee8915681e82ce
ackasaber <ackasaber@gmail.com>
2021-10-18 23:32:49 +0300

    Added the example for "A Simple Web Application" chapter. Now with Jakarta Servlet 5.0 and Jetty 11.

---
+++ simple-webapp/src/main/java/mavenbook/web/SimpleServlet.java
@@ -0,0 +1,20 @@
+package mavenbook.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("Simple servlet executed!");
+        }
+    }
+}
